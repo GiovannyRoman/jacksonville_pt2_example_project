fb106a7e22c8eb9cef5ee9bd03175dd8c02b8152
Michael Boren <mboren@cooksys.com>
2016-07-05 14:39:15 -0500

    initial part 2 example proj

---
+++ src/main/java/com/cooksys/ExampleApplication.java
@@ -0,0 +1,26 @@
+package com.cooksys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@Configuration
+@RestController
+public class ExampleApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+		
+	@RequestMapping("/user")
+	String helloSpring()
+	{
+		return "Michael Boren";
+	}
+}
