2c172115722aa7ed9d7ef27bebf098accc691890
Michael Boren <mboren@cooksys.com>
2016-07-11 16:14:33 -0400

    removed unecessary @configuration annotation, @SpringBootApplication has it by default

--- src/main/java/com/cooksys/ExampleApplication.java
+++ src/main/java/com/cooksys/ExampleApplication.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import com.cooksys.model.AppUser;
 
 @SpringBootApplication
-@Configuration
 @RestController
 public class ExampleApplication {
 	
